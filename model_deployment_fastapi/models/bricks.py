db7cbe7988135960b76870f168c80aaeefa3f5b7
sara w laptop <sara.yaghoubi93@gmail.com>
2022-12-23 10:35:52 -0330

    fixed some name and space issues, pretty stuff .... :))

--- model_deployment_fastapi/models/bricks.py
+++ model_deployment_fastapi/models/bricks.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
+import torch.nn.functional as functional
+
 
 class DoubleConv(nn.Module):
     """(convolution => [BN] => ReLU) * 2"""
@@ -23,27 +24,27 @@ class DoubleConv(nn.Module):
 
 
 class Down(nn.Module):
-    """Downscaling with maxpool then double conv"""
+    """Downscaling with max pool then double conv"""
 
     def __init__(self, in_channels, out_channels, k):
         super().__init__()
-        self.maxpool_conv = nn.Sequential(
+        self.max_pool_conv = nn.Sequential(
             # nn.MaxPool2d(2),
             DoubleConv(in_channels, out_channels, None, k)
         )
 
     def forward(self, x):
-        return self.maxpool_conv(x)
+        return self.max_pool_conv(x)
 
 
 class Up(nn.Module):
-    """Upscaling then double conv"""
+    """Up scaling then double conv"""
 
-    def __init__(self, in_channels, out_channels, bilinear=True, k=(2, 2)):
+    def __init__(self, in_channels, out_channels, bi_linear=True, k=(2, 2)):
         super().__init__()
 
-        # if bilinear, use the normal convolutions to reduce the number of channels
-        if bilinear:
+        # if bi_linear, use the normal convolutions to reduce the number of channels
+        if bi_linear:
             self.up = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)
             self.conv = DoubleConv(in_channels, out_channels, in_channels // 2, kernel_size=k)
         else:
@@ -56,41 +57,38 @@ class Up(nn.Module):
         diffY = x2.size()[2] - x1.size()[2]
         diffX = x2.size()[3] - x1.size()[3]
 
-        x1 = F.pad(x1, [diffX // 2, diffX - diffX // 2,
-                        diffY // 2, diffY - diffY // 2])
-        # if you have padding issues, see
-        # https://github.com/HaiyongJiang/U-Net-Pytorch-Unstructured-Buggy/commit/0e854509c2cea854e247a9c615f175f76fbb2e3a
-        # https://github.com/xiaopeng-liao/Pytorch-UNet/commit/8ebac70e633bac59fc22bb5195e513d5832fb3bd
+        x1 = functional.pad(x1, [diffX // 2, diffX - diffX // 2,diffY // 2, diffY - diffY // 2])
         x = torch.cat([x2, x1], dim=1)
         return self.conv(x)
 
 
 class OutConv(nn.Module):
 
-        def __init__(self, in_channels, out_channels, k_size=(4, 2), nodes=1600):
-            super(OutConv, self).__init__()
-            self.conv = nn.Sequential(
-                nn.Conv2d(in_channels, out_channels, kernel_size=k_size),
-                nn.LeakyReLU(),
-                nn.Flatten()
-            )
-            self.lin1 = nn.Sequential(
-                nn.Linear(nodes, 1028),
-                nn.Dropout(0.2),
-                nn.LeakyReLU()
-            )
-            self.lin2 = nn.Sequential(
-                nn.Linear(1028, 512),
-                nn.Dropout(0.2),
-                nn.LeakyReLU(),
-            )
-            self.final = nn.Sequential(
-                nn.Linear(512, out_channels),
-                nn.GELU()
-            )
-        def forward(self, x):
-            x = self.conv(x)
-            x = self.lin1(x)
-            x = self.lin2(x)
-            x = self.final(x)
-            return torch.squeeze(x)
+    def __init__(self, in_channels, out_channels, k_size=(4, 2), nodes=1600):
+        super(OutConv, self).__init__()
+        self.conv = nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, kernel_size=k_size),
+            nn.LeakyReLU(),
+            nn.Flatten()
+        )
+        self.lin1 = nn.Sequential(
+            nn.Linear(nodes, 1028),
+            nn.Dropout(0.2),
+            nn.LeakyReLU()
+        )
+        self.lin2 = nn.Sequential(
+            nn.Linear(1028, 512),
+            nn.Dropout(0.2),
+            nn.LeakyReLU(),
+        )
+        self.final = nn.Sequential(
+            nn.Linear(512, out_channels),
+            nn.GELU()
+        )
+
+    def forward(self, x):
+        x = self.conv(x)
+        x = self.lin1(x)
+        x = self.lin2(x)
+        x = self.final(x)
+        return torch.squeeze(x)
