c61d3e286100576b0602358c07df3c809376dd0f
sara w laptop <sara.yaghoubi93@gmail.com>
2022-12-23 07:26:52 -0330

    Made some correction to the names, lines, ....[pretty]

--- model_deployment_fastapi/models/UNet.py
+++ model_deployment_fastapi/models/UNet.py
@@ -1,22 +1,22 @@
-import torch
-from models.bricks import *
+from bricks import *
+
 
 class UNet30(nn.Module):
-    def __init__(self, n_channels, n_classes, bilinear=False):
+    def __init__(self, n_channels, n_classes, bi_linear=False):
         super(UNet30, self).__init__()
         self.n_channels = n_channels
         self.n_classes = n_classes
-        self.bilinear = bilinear
+        self.bi_linear = bi_linear
         self.inc = DoubleConv(n_channels, 128, None, k=(4,2))
         self.down1 = Down(128, 256,k=(4,1))
         self.down2 = Down(256, 512,k=(4,1))
         self.down3 = Down(512, 1024,k=(4,1))
-        factor = 2 if bilinear else 1
-        self.down4 = Down(1024, 2048 // factor, k = (4,1))
-        self.up1 = Up(2048, 1024 // factor, bilinear, k=(4,2))
-        self.up2 = Up(1024, 512 // factor, bilinear, k=(4,2))
-        self.up3 = Up(512, 256 // factor, bilinear, k=(4,2))
-        self.up4 = Up(256, 128, bilinear, k=(2,2))
+        factor = 2 if bi_linear else 1
+        self.down4 = Down(1024, 2048 // factor, k=(4,1))
+        self.up1 = Up(2048, 1024 // factor, bi_linear, k=(4,2))
+        self.up2 = Up(1024, 512 // factor, bi_linear, k=(4,2))
+        self.up3 = Up(512, 256 // factor, bi_linear, k=(4,2))
+        self.up4 = Up(256, 128, bi_linear, k=(2,2))
         self.outc = OutConv(128, n_classes, (4,2), nodes=37)
 
     def forward(self, x):
@@ -29,5 +29,5 @@ class UNet30(nn.Module):
         x = self.up2(x, x3)
         x = self.up3(x, x2)
         x = self.up4(x, x1)
-        logits = self.outc(x)
-        return logits
+        logit = self.outc(x)
+        return logit
